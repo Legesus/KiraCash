a9aa25f0a0d3135cd9674d1071ef00dad787aa31
Adam <adamnasir24@gmail.com>
2024-03-28 14:43:03 +0800

    Delete unused profile view images

--- app/src/main/java/com/example/kiracash/ProfileView.kt
+++ app/src/main/java/com/example/kiracash/ProfileView.kt
@@ -1,43 +1,57 @@
 package com.example.kiracash
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.kiracash.ui.theme.KiraCashTheme
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 
-class MainActivity : ComponentActivity() {
+class ProfileView : AppCompatActivity () {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            KiraCashTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
+
+        //setContentView(R.layout.view_profile)
+
+        setContent{
+            profile()
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
+    @Preview
+    @Composable
+    private fun profile() {
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .background(color = Color(android.graphics.Color.parseColor("#f2f1f6"))),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ConstraintLayout(
+                Modifier
+                    .height(250.dp)
+                    .background(color = Color(android.graphics.Color.parseColor("#32357a")))) {
+                    val (topImg,profile, title, back, pen) = createRefs()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KiraCashTheme {
-        Greeting("Android")
+                Image(painterResource(id = R.drawable.arc_3), contentDescription = null,
+                    Modifier
+                        .constrainAs(topImg) {
+                            bottom.linkTo(parent.bottom)
+                        }
+                        .fillMaxWidth())
+            }
+        }
     }
 }
